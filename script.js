2ff30d06c8e4a89d86989ed3b4fe0a6aa49966da
Anita <anita.germanova@gmail.com>
2021-12-27 22:10:00 +0200

    change style, message of rounding and divide by 0

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// "use strict";
+
 function func() {
     let num1 = document.getElementById('num1');
     let num2 = document.getElementById('num2');
     let choose = document.getElementById('choose');
     let box = document.getElementById('box');
+    let message = document.getElementById('round');
+    let warning = document.getElementById('warning');
 
     a = Number(num1.value);
     b = Number(num2.value);
@@ -23,9 +27,30 @@ function func() {
             break;
         case 'div':
             if (c == 'div')
-                x = a / b;
+                if (b === 0) {
+                    num2.classList.add('warning');
+                    warning.classList.add('message_warning');
+                    break;
+                } else {
+                    if (num2.classList.contains('warning')) {
+                        num2.classList.remove('warning');
+                        warning.classList.remove('message_warning');
+                        x = a / b;
+                    } else {
+                        x = a / b;
+                    }
+                }
             break;
     }
 
     box.value = Math.round(x);
+    
+    if (Number(box.value) !== x) {
+        message.classList.remove('zero');
+    } else {
+        if (message.classList.contains('zero')) {
+        } else {
+            (message.classList.add('zero'));
+        }
+    }
 }
